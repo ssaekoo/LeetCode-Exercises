c725fbde6fc3ccb5730e61ffb969b8a3c2c456e2
Stephen Saekoo <ssaekoo@gmail.com>
2016-06-02 11:24:02 -0700

    solve 258

---
+++ 258 Add Digits.rb
@@ -0,0 +1,10 @@
+# Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+#
+# For example:
+#
+# Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+def add_digits(num)
+    return 0 if num == 0
+    (num - 1) % 9 + 1
+end
